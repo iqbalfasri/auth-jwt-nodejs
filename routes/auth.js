04c92b1b1fcddbeec85c2bf0cba93495597f1467
Iqbal Fasri <iqbalx64@gmail.com>
2018-11-21 14:27:41 +0700

    autentikasi masih menggunakan dummy data, verify JWT success

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,38 @@ router.get("/", (req, res, next) => {
   });
 });
 
+router.get("/me", (req, res) => {
+  /*
+   *   Format Token
+   *   x-access-token: Bearer <access_token>
+   */
+
+  var bearerHeader = req.headers["x-access-token"];
+  if (typeof bearerHeader === "undefined")
+    return res.status(401).send({
+      auth: false,
+      message: "Harus ada token dulu bosque"
+    });
+
+  const bearer = bearerHeader.split(" ");
+  const bearerToken = bearer[1];
+
+  // Verify jwt / token
+  jwt.verify(bearerToken, "secretkey", (err, authData) => {
+    // Jika token salah
+    if (err) return res.status(403).send({
+      name: err.name,
+      message: "Token salah"
+    });
+    // Jika token bener
+    res.status(200).send({
+      auth: true,
+      data: authData,
+      message: "Berhasil"
+    });
+  });
+});
+
 router.post("/login", (req, res) => {
   // Dummy User
   const user = {
@@ -19,6 +51,7 @@ router.post("/login", (req, res) => {
 
   jwt.sign({ user }, "secretkey", (err, token) => {
     res.json({
+      auth: true,
       token: token
     });
   });
