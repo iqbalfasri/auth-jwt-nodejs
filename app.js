b5b5b0f6535adaa38029bd2c8ea836f376ddc409
Iqbal Fasri <iqbalx64@gmail.com>
2018-11-21 13:57:29 +0700

    Adding body-parser and cors dependencies

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,27 @@
-var express = require('express');
-var jwt = require('jsonwebtoken');
+var express = require("express");
+var jwt = require("jsonwebtoken");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send({
-    message: "Auth"
+    message: "Auth with JWT"
   });
 });
 
-router.post('/login', (req, res) => {
-  // Mock user
+router.post("/login", (req, res) => {
+  // Dummy User
   const user = {
     id: 1,
-    username: 'iqbalfasri',
-    email: 'iqbalx64@gmail.com'
+    username: "iqbalfasri",
+    email: "iqbalx64@gmail.com"
   };
-  jwt.sign({user: user}, 'secretkey', (err, token) => {
+
+  jwt.sign({ user }, "secretkey", (err, token) => {
     res.json({
       token: token
-    })
-  })
-})
+    });
+  });
+});
 
 module.exports = router;
